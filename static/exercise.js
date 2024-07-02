00e53662aea33237721137c25369ead6af0d0f57
名前 <>
2024-07-02 17:59:42 +0900

    reinitialization

--- static/exercise.js
+++
@@ -1,7 +0,0 @@
-const showAnswerButton = document.getElementById("show-answer-button");
-const answer = document.getElementById("answer");
-
-showAnswerButton.onclick = () => {
-  answer.style.display = "block";
-  showAnswerButton.style.display = "none";
-};
