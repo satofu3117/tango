8ef3c93e52de31708921482385e5851abe1f6148
Hinoto17 <teishro@gmail.com>
2024-07-09 17:49:40 +0900

    正誤回数を表示するように

--- static/exercise.js
+++ static/exercise.js
@@ -1,7 +1,22 @@
 const showAnswerButton = document.getElementById("show-answer-button");
 const answer = document.getElementById("answer");
+const correctButton = document.getElementById("correct-button");
+const incorrectButton = document.getElementById("incorrect-button");
+const numOfCorr = document.getElementById("num-of-corr");
+const numOfIncorr = document.getElementById("num-of-incorr");
 
 showAnswerButton.onclick = () => {
   answer.style.display = "block";
   showAnswerButton.style.display = "none";
 };
+
+correctButton.onclick = () => {
+  n = 0;
+  n++;
+  numOfCorr.innerHTML = n;
+};
+incorrectButton.onclick = () => {
+  n = 0;
+  n++;
+  numOfIncorr.innerHTML = n;
+}
